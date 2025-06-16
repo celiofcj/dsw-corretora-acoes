7d752a52dd6e91307fecc369742bb7d493f29cfe
celiofcj <celiofcjunior_cg@hotmail.com>
2025-06-16 01:09:31 -0300

    Classe para erros de validação

---
+++ backend/src/exception/erros.ts
@@ -0,0 +1,9 @@
+export class ErroValidacao extends Error{
+
+    constructor(erros: Array<string>) {
+        const message = erros.reduce((previousValue: string, currentValue: string) => {
+            return `${previousValue}, ${currentValue}`;
+        })
+        super(message);
+    }
+}
